35e6f2ee62ff15482ba453ebe3d60dd9e9f5d55c
sotir3d <sebastian.fueloep@gmail.com>
2018-08-21 14:28:01 +0200

    Level Shapes, Player Trail

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float speed;
+
+    Rigidbody rb;
+    // Use this for initialization
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+        movement = Camera.main.transform.TransformDirection(movement);
+
+        rb.AddForce(movement * speed);
+    }
+}
---
+++ Assets/Scripts/ThirdPersonCamera.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ThirdPersonCamera : MonoBehaviour
+{
+    public Transform lookAt;
+    public Transform camTransform;
+
+    const float Y_ANGLE_MIN = -20f;
+    const float Y_ANGLE_MAX = 50f;
+
+    Camera cam;
+
+    float distance = 7f;
+    float currentX = 0f;
+    float currentY = 0f;
+    float sensitivityX = 1f;
+    float sensitivityY = 0.5f;
+
+    private void Start()
+    {
+        camTransform = transform;
+        cam = Camera.main;
+    }
+
+    private void Update()
+    {
+        currentX += Input.GetAxis("CameraX") * sensitivityX;
+        currentY += Input.GetAxis("CameraY") * sensitivityY;
+
+        currentY = Mathf.Clamp(currentY, Y_ANGLE_MIN, Y_ANGLE_MAX);
+    }
+
+    private void LateUpdate()
+    {
+        Vector3 dir = new Vector3(0, 0, -distance);
+        Quaternion rotation = Quaternion.Euler(currentY, currentX, 0);
+
+        //newPosition.x = Mathf.Lerp(newPosition.x, lookAt.position.x, 0.1f);
+        //newPosition.x = Mathf.Lerp(newPosition.y, lookAt.position.y, 0.1f);
+
+        camTransform.position = lookAt.position + rotation * dir;
+        camTransform.LookAt(lookAt.position);
+    }
+}
